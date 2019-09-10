e681cade8209268696ddbb9c508e79d90e2f6f12
Feranic <feranic@yandex.ru>
2019-09-10 17:48:17 +0300

    Удалось отправить push-уведомление в системный трей по щелчку кнопки. Пока без иконки. Проверено на Семёрке

--- application.py
+++ application.py
@@ -1,3 +1,4 @@
+from plyer import notification
 from PyQt5 import QtWidgets
 import MyMainWindow
 import sys
@@ -8,5 +9,14 @@ class MyApp(QtWidgets.QMainWindow):
         super().__init__()
         self.ui = MyMainWindow.Ui_MainWindow()
         self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.send_notification)
+
+    def send_notification(self):
+        notification.notify(
+            title = 'Мое уведомление',
+            message = 'Система уведомляет Вас о бренности бытия.',
+            app_name = 'PayPlanner'
+        )
+
 
 
