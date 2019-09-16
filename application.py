3d164c83862aa0dde8e19016da8356460aed1aa1
Feranic <feranic@yandex.ru>
2019-09-16 16:30:15 +0300

    Изменил изначальную структуру проекта

--- application.py
+++ application.py
@@ -1,15 +1,47 @@
 from plyer import notification
-from PyQt5 import QtWidgets
-import MyMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QGridLayout, QWidget, QTableWidget,QTableWidgetItem
+from PyQt5.QtCore import QSize, Qt
 import sys
 
 
-class MyApp(QtWidgets.QMainWindow):
+class MyApp(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.ui = MyMainWindow.Ui_MainWindow()
-        self.ui.setupUi(self)
-        self.ui.pushButton.clicked.connect(self.send_notification)
+        self.setMinimumSize(QSize(480, 80))  # Устанавливаем размеры
+        self.setWindowTitle("Работа с QTableWidget")  # Устанавливаем заголовок окна
+        central_widget = QWidget(self)  # Создаём центральный виджет
+        self.setCentralWidget(central_widget)  # Устанавливаем центральный виджет
+
+        grid_layout = QGridLayout()  # Создаём QGridLayout
+        central_widget.setLayout(grid_layout)  # Устанавливаем данное размещение в центральный виджет
+
+        table = QTableWidget(self)  # Создаём таблицу
+        table.setColumnCount(3)  # Устанавливаем три колонки
+        table.setRowCount(1)  # и одну строку в таблице
+
+        # Устанавливаем заголовки таблицы
+        table.setHorizontalHeaderLabels(["Header 1", "Header 2", "Header 3"])
+
+        # Устанавливаем всплывающие подсказки на заголовки
+        table.horizontalHeaderItem(0).setToolTip("Column 1 ")
+        table.horizontalHeaderItem(1).setToolTip("Column 2 ")
+        table.horizontalHeaderItem(2).setToolTip("Column 3 ")
+
+        # Устанавливаем выравнивание на заголовки
+        table.horizontalHeaderItem(0).setTextAlignment(Qt.AlignLeft)
+        table.horizontalHeaderItem(1).setTextAlignment(Qt.AlignHCenter)
+        table.horizontalHeaderItem(2).setTextAlignment(Qt.AlignRight)
+
+        # заполняем первую строку
+        table.setItem(0, 0, QTableWidgetItem("Text in column 1"))
+        table.setItem(0, 1, QTableWidgetItem("Text in column 2"))
+        table.setItem(0, 2, QTableWidgetItem("Text in column 3"))
+
+        # делаем ресайз колонок по содержимому
+        table.resizeColumnsToContents()
+
+        grid_layout.addWidget(table, 0, 0)  # Добавляем таблицу в сетку
+
 
     def send_notification(self):
         notification.notify(
