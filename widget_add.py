4496153b7927b627a94cacd985a031f7697ac4af
Feranic <feranic@yandex.ru>
2019-12-10 18:45:01 +0300

    Крошечный рефакторинг, и изменена БД.

--- widget_add.py
+++ widget_add.py
@@ -8,10 +8,11 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
+
 class Ui_Form(object):
     def setupUi(self, Form):
-        Form.setObjectName("Form")
-        Form.resize(356, 277)
+        self.setObjectName("Form")
+        self.resize(356, 277)
         self.label = QtWidgets.QLabel(Form)
         self.label.setGeometry(QtCore.QRect(20, 10, 131, 21))
         self.label.setObjectName("label")
